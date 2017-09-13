52c9a6f055bdedda157fcb5e105990119497042e
Tamashiro Kakeru <k-tamashiro@tads.co.jp>
2017-09-13 20:21:23 +0900

    change ldaAnalysis_3_6.py

--- ldaAnalysis_3_6.py
+++ ldaAnalysis_3_6.py
@@ -7,6 +7,9 @@ import gensim
 import collections
 from wordcloud import WordCloud
 import multiprocessing as mp
+import numpy as np
+from scipy.spatial import distance
+from sklearn.cluster import KMeans
 
 def create_gensim_dictionary(data_path, mecab_path=None, no_below=2, no_above=0.1):
 
@@ -80,6 +83,73 @@ def lda(dictionary, corpus, corpus_tfidf, lda_model=None, save_name="test.model"
         topic_terms.append(lda_model.get_topic_terms(id, topn=20))
 
     return corpus_lda_tfidf, topic_terms
+
+def topics2feature(corpus_lda_tfidf, num_topics):
+
+    print("# Start topics to feature dense")
+    dense = np.zeros((len(corpus_lda_tfidf), num_topics), float)
+    doc2id = {}
+    id2doc = {}
+    i = 0
+    for doc_id, topics in corpus_lda_tfidf.items():
+        doc2id[doc_id] = i
+        id2doc[i] = doc_id
+        for topic_id, vec in topics:
+            dense[i, topic_id] = vec
+        i += 1
+
+    pairwise = distance.squareform(distance.pdist(dense))
+    largest = pairwise.max()
+    for doc_id in range(len(corpus_lda_tfidf)):
+        pairwise[doc_id, doc_id] = largest + 1
+
+    return dense, pairwise, doc2id, id2doc
+
+def kmeans(dense, doc2id, id2doc, start, end):
+
+    print("# Start KMeans clustering")
+    cluster_result = {}
+    kmeans_inertia = []
+    for cluster in range(start, end):
+        cluster_result[cluster] = {}
+        kmeans_model = KMeans(n_clusters=cluster,
+                              init="k-means++",
+                              n_init=10,
+                              max_iter=300,
+                              tol=1e-04,
+                              random_state=0)
+        kmeans_model.fit(dense)
+        labels = kmeans_model.labels_
+        kmeans_inertia.append(kmeans_model.inertia_)
+
+        with open("Data/clustering/clustering_{0}_result.txt".format(cluster), "w") as f:
+            f.write("clustering {0} evaluation value : {1} \n".format(cluster, kmeans_inertia[cluster-2]))
+            for id, label in enumerate(labels):
+                if label not in cluster_result[cluster].keys():
+                    cluster_result[cluster][label] = []
+                cluster_result[cluster][label].append(id2doc[id])
+                f.write("{0} is cluster {1} \n".format(id2doc[id], label))
+
+        with open("Data/clustering/clustering_result_{0}_list.txt".format(cluster), "w") as f:
+            f.write("clustering {0} result list\n".format(cluster))
+            for cluster, docs in cluster_result[cluster].items():
+                f.write("\n\n[cluster {0}] : \n".format(cluster))
+                for doc in docs:
+                    f.write("{0}, ".format(doc))
+
+    return cluster_result, kmeans_inertia
+
+def closest_to(doc_ids, pairwise, doc2id, id2doc):
+
+    print("# Start closest to doc")
+    closest_doc_list = {}
+    with open("closet_doc_list.txt", "w") as f:
+        for doc_id in doc_ids:
+            closest_doc = id2doc[pairwise[doc2id[doc_id]].argmin()]
+            closest_doc_list[doc_id] = closest_doc
+            f.write("{0}　：　{1} \n".format(doc_id, closest_doc))
+
+    return closest_doc_list
 
 def doc2topic_id(corpus_lda_tfidf):
 
@@ -166,37 +236,53 @@ def subprocess(ini, fin):
         _, topic_freq_word, topic_terms_20_above = word_cloud_list(dictionary, corpus_tfidf, docs_topic, topic_terms)
         create_wordcloud(topic_freq_word, topic_terms_20_above, cloud_name, "/usr/share/fonts/opentype/ipafont-gothic/ipag.ttf")
 
-if __name__ == "__main__":
+def multiprocess_1(subprocess, arg1, arg2, arg3):
 
-    print("# start")
-    docs, dictionary = create_gensim_dictionary("/home/tamashiro/AI/OPC/LDAPython/Data/対応方法", mecab_path=" -d /usr/lib/mecab/dic/mecab-ipadic-neologd")
-    corpus, corpus_tfidf = create_gensim_corpus(docs, dictionary)
-
+    print("# Start Multi process")
     ps = [
-        mp.Process(target=subprocess, args=(2, 26)),
-        mp.Process(target=subprocess, args=(26, 41)),
-        mp.Process(target=subprocess, args=(41, 56)),
-        mp.Process(target=subprocess, args=(56, 71)),
-        mp.Process(target=subprocess, args=(71, 81)),
-        mp.Process(target=subprocess, args=(81, 91)),
-        mp.Process(target=subprocess, args=(91, 96)),
-        mp.Process(target=subprocess, args=(96, 101))]
+        mp.Process(target=subprocess, args=(arg1, arg2, arg3, 2, 11)),
+        mp.Process(target=subprocess, args=(arg1, arg2, arg3, 11, 21)),
+        mp.Process(target=subprocess, args=(arg1, arg2, arg3, 21, 31)),
+        mp.Process(target=subprocess, args=(arg1, arg2, arg3, 31, 41)),
+        mp.Process(target=subprocess, args=(arg1, arg2, arg3, 41, 51)),
+        mp.Process(target=subprocess, args=(arg1, arg2, arg3, 51, 61)),
+        mp.Process(target=subprocess, args=(arg1, arg2, arg3, 61, 71)),
+        mp.Process(target=subprocess, args=(arg1, arg2, arg3, 71, 81)),
+        mp.Process(target=subprocess, args=(arg1, arg2, arg3, 81, 91)),
+        mp.Process(target=subprocess, args=(arg1, arg2, arg3, 91, 101))]
 
     for p in ps:
         p.start()
 
-    """
+def multiprocess_2(subprocess):
+
+    print("# Start Multi process")
     L = 100
     proc = 10
     pool = mp.Pool(proc)
-    pool.map(subproc, range(10))
+    pool.map(subprocess, range(10))
 
-    for num_topics in range(6, 101):
-        lda_model = "Data/model/model_topic{0}.lda".format(num_topics)
-        cloud_name = "Data/wordcloud/wordcloud_{0}_topic".format(num_topics)
-        corpus_lda_tfidf, topic_terms = lda(dictionary, corpus, corpus_tfidf, save_name=lda_model, num_topics=num_topics)
-        #corpus_lda_tfidf, topic_terms = lda(dictionary, corpus, corpus_tfidf, lda_model=lda_model, num_topics=num_topics)
-        docs_topic = doc2topic_id(corpus_lda_tfidf)
-        _, topic_freq_word, topic_terms_20_above = word_cloud_list(dictionary, corpus_tfidf, docs_topic, topic_terms)
-        create_wordcloud(topic_freq_word, topic_terms_20_above, cloud_name, "/usr/share/fonts/opentype/ipafont-gothic/ipag.ttf")
-    """
+
+
+if __name__ == "__main__":
+
+    print("# start")
+    num_topics = 100
+    lda_model = "Data/model/model_topic{0}.lda".format(num_topics)
+    cloud_name = "Data/wordcloud/wordcloud_{0}_topic".format(num_topics)
+
+    docs, dictionary = create_gensim_dictionary("/home/tamashiro/AI/OPC/LDAPython/Data/対応方法", mecab_path=" -d /usr/lib/mecab/dic/mecab-ipadic-neologd")
+    corpus, corpus_tfidf = create_gensim_corpus(docs, dictionary)
+
+    #corpus_lda_tfidf, topic_terms = lda(dictionary, corpus, corpus_tfidf, save_name=lda_model, num_topics=num_topics)
+    corpus_lda_tfidf, topic_terms = lda(dictionary, corpus, corpus_tfidf, lda_model=lda_model, num_topics=num_topics)
+
+    dense, pairwise, doc2id, id2doc = topics2feature(corpus_lda_tfidf, num_topics)
+    closest_doc_list = closest_to(corpus_lda_tfidf.keys(), pairwise, doc2id, id2doc)
+    cluster_result = kmeans(dense, doc2id, id2doc, 2, 101)
+    #multiprocess_1(kmeans, dense, doc2id, id2doc) 
+
+    docs_topic = doc2topic_id(corpus_lda_tfidf)
+
+    _, topic_freq_word, topic_terms_20_above = word_cloud_list(dictionary, corpus_tfidf, docs_topic, topic_terms)
+    create_wordcloud(topic_freq_word, topic_terms_20_above, cloud_name, "/usr/share/fonts/opentype/ipafont-gothic/ipag.ttf")
